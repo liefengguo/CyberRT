ce621b3d96354e867fb050f89eeb6cc011c56f4d
liefengguo <1321755404@qq.com>
2025-07-27 12:31:06 +0800

    解决无法链接到cdr

--- install.py
+++ install.py
@@ -551,8 +551,8 @@ class Install:
         self._cmd("mkdir -p build")
         os.chdir("build")
         self._cmd(
-            "cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
-                self._install_prefix
+            "cmake -DCMAKE_INSTALL_PREFIX={} -DCMAKE_PREFIX_PATH={} -DBUILD_SHARED_LIBS=ON ..".format(
+                self._install_prefix,self._install_prefix
             )
         )
         self._cmd("make install -j$(($(nproc) - 1))")
